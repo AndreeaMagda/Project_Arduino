84a236e40f5cfc5d60a4e56206acf1a8de990a04
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2023-04-26 14:03:02 +0300

    something strange
    at first: all leds are on and L
    if i push the first button: sall (without L) appears on the display, the buzzer bip 1/2s and all leds are off

--- proiect/proiect.ino
+++ proiect/proiect.ino
@@ -1,6 +1,6 @@
 
-unsigned char numbers[4] = { 0x92, 0x88, 0xC7, 0xC7 };  //fOr display
-unsigned char digits[4] = { 0x01, 0x02, 0x04, 0x08 };   //for display
+unsigned char numbers[4] = { 0x92, 0x88, 0xC7, 0xC7 };                             //fOr display
+unsigned char digits[4] = { 0x01, 0x02, 0x04, 0x08 };                              //for display
 unsigned char first_state[] = { 0xDF, 0xCF, 0xEF, 0XE7, 0xF7, 0XF3, 0xFB, 0XDB };  //for motor
 unsigned char i = 0;
 
@@ -13,13 +13,14 @@ void setupMotor() {
   noInterrupts();
   DDRB = 0X3C;
   DDRC = 0x00;
-  DDRD=0X00;
+  DDRD = 0X00;
   TCCR1A = 0X00;
   TCCR1B = 0X1D;
   TIMSK1 |= (1 << OCIE1A);
   OCR1A = (16000000 / 1024) * 1;
   TCNT1 = 0X00;
   interrupts();
+  DDRD |= (1 << 3);  //buzzer
 }
 
 void displaySegments() {
@@ -32,16 +33,28 @@ void displaySegments() {
 ISR(TIMER1_COMPA_vect) {
 
   TCNT1 = 0;
-  PORTB = first_state[i];{
-  i++;
-  if (i == 8) {
-    i = 0;
-  }
+  PORTB = first_state[i];
+  {
+    i++;
+    if (i == 8) {
+      i = 0;
+    }
+    
   }
 }
 
-ISR(PCINT8_vect){}
-
+ISR(PCINT9_vect) {
+  for (int i = 0; i < 60; i++) {  // repeat for 60 cycles (1 minute)
+    PORTD |= (1 << 3);            // turn the buzzer on using bit manipulation
+    delay(100);                   // wait for 0.1 seconds
+    PORTD &= ~(1 << 3);           // turn the buzzer off using bit manipulation
+    delay(100);                   // wait for 0.1 seconds
+  }
+  delay(60000);  // wait for 1 minute before repeating the cycle}
+}
+ISR(PCINT10_vect) {
+   displaySegments();
+}
 
 void SetLatch() {
   PORTD = PORTD | 0x10;
@@ -82,16 +95,19 @@ void Display(unsigned char digit, unsigned char number) {
 int main() {
   PORTB = 0Xff;
   PORTC = 0x02;
+  PORTD=0X00;
 
  
-
   while (1) {
-     if ((PINC & 0x02) == 0x02) {
+    if ((PINC & 0x02) == 0x02) {
+
+      setupMotor();
+      
     
-   setupMotor();
-   }else {
-    setup();
-    displaySegments();
+    } else if((PINC & 0x04) != 0x04){
+      setup();
+      displaySegments();
+      
+    }
   }
   }
-}
